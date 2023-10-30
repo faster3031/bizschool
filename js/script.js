68243227208236bfc102267c3122453084637d80
faster3031 <dongoh3031@gmail.com>
2023-10-30 12:35:59 +0900

    feat : 가로 스크롤 기능

--- js/script.js
+++ js/script.js
@@ -30,33 +30,32 @@ window.onload = function () {
         spaceBetween: 30,
         autoplay:true,
       });
-      const wrap = document.querySelector(".main-tab-wrap");
-      const sliderContainer = document.querySelector(".main-tab")
-      wrap.addEventListener("mousedown" ,(e) => {
-        wrap.style.cursor = "grabbing"
-        sliderContainer.style.userSelect = "none"//글자 선택방지
-        sliderContainer.style.pointerEvents = "none"//다른 이벤트 무시
-
-        // 드래그 중에 마우스 이동을 기록하는데 사용됨
-        let startX = e.pageX;
-        // 현재 슬라이더 컨테이너의 가로 스크롤 위치를 지정
-        let scrollLeft = sliderContainer.scrollLeft
-        // 드래그 동작 중에 슬라이더를 움직이는 역할
-        function mousemove(e){
-          // 현재 마우스 위치와 클릭한 위치 사이의 차이를 계산하여
-          // 드래그 중에 이동해야 할 거리
-          const x = e.pageX - startX;
-          // 슬라이더 컨테이너의 가로 스크롤 위치를 업데이트하여
-          // 드래그 동작에 따라 슬라이더를 이동합니다
-          sliderContainer.scrollLeft = scrollLeft - x;
-        }
-        wrap.addEventListener("mousemove" , mousemove)
-        wrap.addEventListener("mouseup" , () => {
-          wrap.style.cursor = "grab"
-          sliderContainer.style.scrollBehavior = 'smooth';
-          sliderContainer.style.userSelect = "auto"//글자 선택방지
-          sliderContainer.style.pointerEvents = "auto"//다른 이벤트 무시
-          wrap.removeEventListener("mousemove", mousemove)
-        })
-      })
+      let isDragging = false;
+      let startX, scrollLeft;
+      
+      const mainTab = document.querySelector('.main-tab-box');
+      
+      mainTab.addEventListener('mousedown', (e) => {
+          isDragging = true;
+          startX = e.pageX - mainTab.offsetLeft;
+          scrollLeft = mainTab.scrollLeft;
+      });
+      
+      mainTab.addEventListener('mouseup', () => {
+          isDragging = false;
+      });
+      
+      mainTab.addEventListener('mouseleave', () => {
+          isDragging = false;
+      });
+      
+      mainTab.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+        e.preventDefault();
+        requestAnimationFrame(() => {
+            const x = e.pageX - mainTab.offsetLeft;
+            const walk = (x - startX) * 2; // 조절 가능한 스크롤 속도
+            mainTab.scrollLeft = scrollLeft - walk;
+        });
+    });
 };
